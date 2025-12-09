f157a7a5c416a00457abd753030827e0420fd3e1
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2025-12-10 00:28:12 +0100

    feat: Configurer le point d'entrée 'server.js'

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import 'dotenv/config';
+import dbConnection from './database/ConnexionDB.js';
+
+const app = express();
+const uri = process.env.MONGO_URI;
+const port = process.env.PORT;
+
+app.get('/', (req, res)=>{
+    res.send('Bienvenu chez RoadSync');
+});
+
+const startServer = async () => {
+    try {
+        await dbConnection.connectToDb(uri);
+        
+        app.listen(port, () => {
+            console.info(`Server is running on : http://localhost:${port}/`);
+        });
+    } catch (e) {
+        console.error(`Erreur de connexion avec la base de donnée : ${e.message}`);
+        process.exit(1);
+    }
+
+}
+
+startServer();

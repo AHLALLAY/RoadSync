209e7d5d87a0cc354a33cd55d4398f01731c8627
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2025-12-09 22:15:56 +0100

    feat: Ajouter la classe de connexion au base de donnée

---
+++ backend/database/ConnexionDB.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+
+class Connection {
+    constructor() {
+        this.isConnected = false;
+    }
+
+    async connectToDb(uri) {
+        if (!uri) {
+            console.error('Erreur Fatale : MONGO_URI n\'est pas défini dans le .env');
+            process.exit(1);
+        }
+
+        if (this.isConnected) {
+            console.log('Base de données déjà connectée');
+            return;
+        }
+
+        try {
+            const conn = await mongoose.connect(uri);
+            
+            this.isConnected = true;
+            console.log(`MongoDB Connecté : ${conn.connection.host}`);
+            
+            return conn;
+        } catch (error) {
+            console.error(`Erreur de connexion MongoDB : ${error.message}`);
+            process.exit(1);
+        }
+    }
+}
+
+export default new Connection();

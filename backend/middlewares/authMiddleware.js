fdf8d1671fc983d24c5760c3217b22f43aaf6fca
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2025-12-10 16:28:32 +0100

    feat: Ajouter 'AuthMiddleware' avec la méthode 'verifyToken'

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,24 @@
+import returns from '../utils/returns.js';
+import jwt from 'jsonwebtoken';
+
+class AuthMiddleware {
+    
+    verifyToken(req, res, next) {
+        const authHeader = req.headers['authorization'];
+        const token = authHeader && authHeader.split(' ')[1];
+
+        if (!token) {
+            return returns(res, 403, false, "Accès refusé. Aucun token fourni.");
+        }
+
+        try {
+            const userData = jwt.verify(token, process.env.JWT_SECRET);
+            req.user = userData;
+            next();
+        } catch (e) {
+            return returns(res, 401, false, "Token invalide ou expiré.");
+        }
+    }
+}
+
+export default new AuthMiddleware();

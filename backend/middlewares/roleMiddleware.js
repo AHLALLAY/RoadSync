3213d7f0e980082ad62d60fa35146b6f5c945a4e
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2025-12-11 17:11:55 +0100

    fix: Réparer l'incohérence entre 'UserModel' et 'RoleMiddleware'

--- backend/middlewares/roleMiddleware.js
+++ backend/middlewares/roleMiddleware.js
@@ -4,14 +4,14 @@ class RoleMiddleware {
 
     isAdmin(req, res, next) {
         if (!req.user || !req.user.role) return returns(res, 401, false, "Utilisateur non identifié.");
-        if (req.user.role === 'admin') return next();
+        if (req.user.role === 'Admin') return next();
 
         return returns(res, 403, false, "Accès refusé. Droits administrateur requis.");
     }
 
     isDriver(req, res, next) {
         if (!req.user || !req.user.role) return returns(res, 401, false, "Utilisateur non identifié.");
-        if (req.user.role === 'driver') return next();
+        if (req.user.role === 'Chauffeur') return next();
 
         return returns(res, 403, false, "Accès refusé. Espace réservé aux chauffeurs.");
     }

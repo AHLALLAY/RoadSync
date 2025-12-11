2a5dd78c1d06b22aaddb3ef26ba777f8a3410f46
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2025-12-11 11:33:08 +0100

    refact: Renommer les champs en anglais

--- backend/models/TruckModel.js
+++ backend/models/TruckModel.js
@@ -1,44 +1,44 @@
 import mongoose from 'mongoose';
 
 const truckSchema = new mongoose.Schema({
-    matricule: {
+    registrationNumber: { 
         type: String,
         required: [true, "Le matricule est requis"],
         unique: true,
         trim: true
     },
-    marque: {
+    make: { 
         type: String,
         required: [true, "La marque est requise"],
         trim: true
     },
-    modele: {
+    model: {
         type: String,
         required: [true, "Le modèle est requis"],
         trim: true
     },
-    annee: {
+    year: {
         type: Number,
         required: [true, "L'année de mise en service est requise"],
     },
     status: {
         type: String,
-        enum: ['Disponible', 'En voyage', 'Maintenance'],
+        enum: ['Disponible', 'En voyage', 'Maintenance'], 
         default: 'Disponible'
     },
-    compteurKm: {
+    mileage: { 
         type: Number,
         required: [true, "Le kilométrage est requis"],
         min: [0, "Le kilométrage ne peut pas être négatif"]
     },
-    capaciteReservoir: {
+    tankCapacity: {
         type: Number,
         required: [true, "La capacité du réservoir est requise"],
         min: [0, "La capacité ne peut pas être négative"]
     },
-    typeCarburant: {
+    fuelType: {
         type: String,
-        enum: ['Diesel', 'Essence', 'Hybride', 'Electrique'],
+        enum: ['Diesel', 'Essence', 'Hybride', 'Electrique'], 
         default: 'Diesel'
     },
 }, {

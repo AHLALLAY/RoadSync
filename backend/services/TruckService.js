e3fd98e1641416376d270088c744bbac156ad1ff
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2025-12-11 14:37:24 +0100

    feat: Ajouter 'TruckService' avec la méthode 'createTruck'

---
+++ backend/services/TruckService.js
@@ -0,0 +1,15 @@
+import Truck from "../models/TruckModel";
+
+class TruckService {
+    async createTruck(truckData) {
+        const isExiste = await Truck.findOne({registrationNumber: truckData.registrationNumber});
+
+        if(isExiste) throw new Error(`Le camion avec le matricule ${truckData.registrationNumber} existe déjà.`);
+        
+        const newTruck = await Truck.create(truckData);
+        
+        return newTruck;
+    }
+}
+
+export default new TruckService();

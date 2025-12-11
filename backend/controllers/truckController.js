5e5a21ed01bae4fe7570d0457b3d53ca33d13791
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2025-12-11 17:08:45 +0100

    feat: Ajouter 'TruckController' avec la méthode 'createTruck'

---
+++ backend/controllers/truckController.js
@@ -0,0 +1,27 @@
+import truckServise from '../services/truckService.js';
+import returns from '../utils/returns.js';
+
+class TruckController {
+    async createTruck(req, res) {
+        try {
+            const result = await truckServise.createTruck(req.body);
+            return returns(res, 201, true, "L'ajout du camion a réussi", result);
+        } catch (e) {
+            if (e.name === 'ValidationError') {
+                const firstErrorMessage = Object.values(e.errors).map(val => val.message)[0];
+                return returns(res, 400, false, firstErrorMessage);
+            }
+            if (e.code === 11000) {
+                return returns(res, 400, false, "Ce véhicule (matricule) existe déjà.");
+            }
+
+            if (e.message.includes("existe déjà") || e.message.includes("Veuillez remplir")) {
+                return returns(res, 400, false, e.message);
+            }
+
+            return returns(res, 500, false, "Erreur serveur inattendue", null, e.message);
+        }
+    }
+}
+
+export default new TruckController();

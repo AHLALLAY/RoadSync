7918e4f8f9702ab755dbf5a630845fb1e8ed6513
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2025-12-11 21:36:58 +0100

    feat: Ajouter la route vers 'readeTrucks'

--- backend/routes/truckRoutes.js
+++ backend/routes/truckRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import authMiddleware from '../middlewares/authMiddleware.js';
 import roleMiddleware from '../middlewares/roleMiddleware.js';
-import truckController from '../controllers/truckController.js'; 
+import TruckController from '../controllers/TruckController.js'; 
 
 const router = express.Router();
 
-router.post('/truck', authMiddleware.verifyToken, roleMiddleware.isAdmin, truckController.createTruck.bind(truckController));
+router.post('/truck', authMiddleware.verifyToken, roleMiddleware.isAdmin, TruckController.createTruck);
+router.get('/trucks', authMiddleware.verifyToken, roleMiddleware.isAdmin, TruckController.readeTrucks);
 
 export default router;

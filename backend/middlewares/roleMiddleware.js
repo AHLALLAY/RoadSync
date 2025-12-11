f1a7165e55073bdb2e10e42993b0c8d07200f301
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2025-12-11 11:19:21 +0100

    feat: Ajouter 'roleMiddleware.js'

---
+++ backend/middlewares/roleMiddleware.js
@@ -0,0 +1,34 @@
+import returns from "../utils/returns";
+
+class RoleMiddleware {
+
+    isAdmin(req, res, next) {
+        if (!req.user || !req.user.role) return returns(res, 401, false, "Utilisateur non identifié.");
+        if (req.user.role === 'admin') return next();
+
+        return returns(res, 403, false, "Accès refusé. Droits administrateur requis.");
+    }
+
+    isDriver(req, res, next) {
+        if (!req.user || !req.user.role) return returns(res, 401, false, "Utilisateur non identifié.");
+        if (req.user.role === 'driver') return next();
+
+        return returns(res, 403, false, "Accès refusé. Espace réservé aux chauffeurs.");
+    }
+
+    check(requiredRole) {
+        return (req, res, next) => {
+            if (!req.user || !req.user.role) {
+                 return returns(res, 401, false, "Utilisateur non identifié.");
+            }
+
+            if (req.user.role !== requiredRole) {
+                return returns(res, 403, false, `Accès refusé. Rôle ${requiredRole} requis.`);
+            }
+            
+            next();
+        };
+    }
+}
+
+export default new RoleMiddleware();

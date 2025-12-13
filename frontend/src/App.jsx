a010d7fa1954a983bd8c417621e82434de593d37
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2025-12-13 15:30:22 +0100

    feat: Ajouter les routes dans 'App.jsx'

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './layout/layout';
+import Login from './pages/auth/login';
+
 
 function App() {
-
   return (
-    <div>
-
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route path="login" element={<Login />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App
+export default App;

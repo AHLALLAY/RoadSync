c3aa5b65a35b566220c173d187d2304967cfddb3
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2025-12-14 18:25:31 +0100

    fix: Installer et ajouter le middleware pour 'CORS'

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import 'dotenv/config';
+import cors from 'cors';
 import dbConnection from './database/ConnexionDB.js';
 import authRoutes from './routes/authRoutes.js';
 import truckRoutes from './routes/truckRoutes.js';
@@ -8,19 +9,20 @@ const app = express();
 const uri = process.env.MONGO_URI;
 const port = process.env.PORT;
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
-app.use('/api/admin/',truckRoutes);
+app.use('/api/admin/', truckRoutes);
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('Bienvenu chez RoadSync');
 });
 
 const startServer = async () => {
     try {
         await dbConnection.connectToDb(uri);
-        
+
         app.listen(port, () => {
             console.info(`Server is running on : http://localhost:${port}/`);
         });
